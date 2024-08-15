50127908ecc5c04bfd78f9fbcb249c4f337d60ff
andregsistemas <andregsistemas@outlook.com>
2024-08-15 13:41:17 -0300

    05. Build e deploy

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,3 +1,4 @@
+
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RouterOutlet } from '@angular/router';
@@ -6,6 +7,13 @@ import { CabecalhoComponent } from './componentes/cabecalho/cabecalho.component'
 import { SeparadorComponent } from './componentes/separador/separador.component';
 import { ContatoComponent } from "./componentes/contato/contato.component";
 
+interface Contato {
+  id: number
+  nome: string
+  telefone: string
+}
+
+import agenda from './agenda.json'
 @Component({
   selector: 'app-root',
   standalone: true,
@@ -22,4 +30,11 @@ import { ContatoComponent } from "./componentes/contato/contato.component";
 })
 export class AppComponent {
   alfabeto: string = 'abcdefghijklmnopqrstuvwxyz'
+  contatos: Contato[] = agenda
+
+  filtrarContatosPorLetraInicial(letra:string) : Contato[] {
+    return this.contatos.filter(contato => {
+      return contato.nome.toLowerCase().startsWith(letra)
+    })
+  }
 }
